78fcf611800d135cd8b8a2330984da51a77873e0
HelgaAthame <athamethedarkest@gmail.com>
2024-04-22 15:58:52 +0300

    feat: correct tracklist styles

--- front/src/app/tracks/page.tsx
+++ front/src/app/tracks/page.tsx
@@ -1,9 +1,9 @@
-import { TrackHeader } from '@/components/TrackHeader';
-import { TrackItem } from '@/components/TrackItem';
-import { Track } from '@/types/track';
-import { Card, Grid, Divider } from '@mui/material';
+import { TrackHeader } from "@/components/TrackHeader";
+import { TrackItem } from "@/components/TrackItem";
+import { Track } from "@/types/track";
+import { Card, Grid, Divider } from "@mui/material";
 
-export const getTracks = async ():Promise<Track[]> => {
+export const getTracks = async (): Promise<Track[]> => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const res = await fetch(`${baseUrl}/tracks/`);
   return res.json();
@@ -13,15 +13,18 @@ export default async function Tracks() {
   return (
     <Grid container className="pd">
       <Card className="pd grow backdrop-blur bg-white/50 flex flex-col gap-4">
-        <TrackHeader 
-        title="Track list"
-        button={{
-          title: "Upload",
-          link: "/tracks/create"
-        }}/>
-        <Divider />
+        <TrackHeader
+          title="Track list"
+          button={{
+            title: "Upload",
+            link: "/tracks/create",
+          }}
+        />
         <div className="flex flex-col">
-       {tracks.map((track) => (<TrackItem track={track} key={track._id}/>))}</div>
+          {tracks.map((track) => (
+            <TrackItem track={track} key={track._id} />
+          ))}
+        </div>
       </Card>
     </Grid>
   );
--- front/src/components/TrackItem/TrackItem.tsx
+++ front/src/components/TrackItem/TrackItem.tsx
@@ -1,20 +1,18 @@
 "use client";
 import { appStore } from "@/store/store";
-import {Track} from "@/types/track";
-import {Pause, PlayArrow, Photo, Delete} from "@mui/icons-material";
-import {IconButton} from "@mui/material";
+import { Track } from "@/types/track";
+import { Pause, PlayArrow, Photo, Delete } from "@mui/icons-material";
+import { IconButton, Divider } from "@mui/material";
 import Image from "next/image";
-import {useRouter} from "next/navigation";
-import { MouseEventHandler } from "react";
+import { useRouter } from "next/navigation";
+import { type MouseEventHandler, Fragment } from "react";
 
 interface Props {
   track: Track;
   active?: boolean;
 }
-export const TrackItem = ({track}: Props) => {
-  const { active, setTrack, setActive } = appStore(
-    (state: any) => state
-  );
+export const TrackItem = ({ track }: Props) => {
+  const { active, setTrack, setActive } = appStore((state: any) => state);
   const router = useRouter();
 
   const play: MouseEventHandler<HTMLButtonElement> | undefined = (e) => {
@@ -23,34 +21,37 @@ export const TrackItem = ({track}: Props) => {
     setActive(!active);
   };
   return (
-    <div
-      className="odd:backdrop-saturate-50 flex items-center p-2 gap-2"
-      onClick={() => {
-        router.push(`/tracks/${track._id}`);
-      }}
-    >
-      <IconButton onClick={play}>
-        {active ? <PlayArrow /> : <Pause />}
-      </IconButton>
-      {track.picture ? (
-        <div className="w-10 h-10 rounded relative shrink-0 overflow-clip">
-          <Image
-            alt={track.name}
-            src={`${process.env.NEXT_PUBLIC_BASE_URL}/${track.picture}`}
-            objectFit="cover"
-            layout="fill"
-          />
+    <Fragment>
+      <div
+        className="flex items-center p-2 gap-2"
+        onClick={() => {
+          router.push(`/tracks/${track._id}`);
+        }}
+      >
+        <IconButton onClick={play}>
+          {active ? <PlayArrow /> : <Pause />}
+        </IconButton>
+        {track.picture ? (
+          <div className="w-10 h-10 rounded relative shrink-0 overflow-clip">
+            <Image
+              alt={track.name}
+              src={`${process.env.NEXT_PUBLIC_BASE_URL}/${track.picture}`}
+              objectFit="cover"
+              layout="fill"
+            />
+          </div>
+        ) : (
+          <Photo />
+        )}
+        <div className="grow flex flex-col">
+          <div>{track.name}</div>
+          <div>{track.artist}</div>
         </div>
-      ) : (
-        <Photo />
-      )}
-      <div className="grow flex flex-col">
-        <div>{track.name}</div>
-        <div>{track.artist}</div>
+        <IconButton onClick={(e) => e.stopPropagation()}>
+          <Delete />
+        </IconButton>
       </div>
-      <IconButton onClick={(e) => e.stopPropagation()}>
-        <Delete />
-      </IconButton>
-    </div>
+      <Divider className="last:hidden" />
+    </Fragment>
   );
 };
