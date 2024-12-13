39a21930b230ac700cbc207b5f3066ee9c637210
HelgaAthame <athamethedarkest@gmail.com>
2024-12-13 15:28:35 +0300

    feat: add cors headers

--- back/dist/main.js
+++ back/dist/main.js
@@ -23,6 +23,12 @@ const start = async () => {
         swagger_1.SwaggerModule.setup('swagger', app, documentFactory);
         app.use(bodyParser.json({ limit: '40mb' }));
         app.use(bodyParser.urlencoded({ extended: true, limit: '40mb' }));
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', 'https://nest-next-kappa.vercel.app');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+            next();
+        });
         await app.listen(PORT, '0.0.0.0', () => {
             console.log(`Server listens on PORT=${PORT}`);
         });
--- back/src/main.ts
+++ back/src/main.ts
@@ -25,6 +25,19 @@ const start = async () => {
     app.use(bodyParser.json({ limit: '40mb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: '40mb' }));
 
+    app.use((req, res, next) => {
+      res.header(
+        'Access-Control-Allow-Origin',
+        'https://nest-next-kappa.vercel.app',
+      );
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Accept, Authorization',
+      );
+      next();
+    });
+
     await app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server listens on PORT=${PORT}`);
     });
