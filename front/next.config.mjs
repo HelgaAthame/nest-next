45a4a3b92362e5b9766ba8512cdef027b3a4a9d6
HelgaAthame <athamethedarkest@gmail.com>
2024-12-17 14:31:57 +0300

    feat: update image remote patters

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "nest-next-production.up.railway.app",
-        pathname: "/**",
+        pathname: "/image/**",
       },
     ],
   },
