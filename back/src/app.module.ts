dcecf78833aa0a96a96c964e29ff4ce2638296f0
olga <khmarukolgaivanovna@gmail.com>
2026-07-14 11:32:32 -0400

    Remove dead ServeStaticModule config (no static folder existed, was intercepting API routes with SPA fallback 404s)

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,15 +1,10 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
-import { resolve } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { AlbumModule } from './album/album.module';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: resolve(__dirname, 'static'),
-    }),
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
     FileModule,
     AlbumModule,
